b7b44ba3eae9fefa4d4d86da6381f1fb82408a06
skmaru <s_maruyama7@yahoo.co.jp>
2022-02-02 22:59:09 +0900

    ADD: Taskのindex、createアクション
    タスク一覧を表示させるためのindexアクションを追加。さらにcreateアクションを追加。indexページに新規作成フォームを実装予定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,20 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path, success: t('defaults.messages.created', item: Task.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.messages.not_created', item: Task.model_name.human)
+    end
+  end 
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :status)
   end
 end
