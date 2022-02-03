c5d9f1d9216d3b89f917dbe9d70198fa2857ba65
skmaru <s_maruyama7@yahoo.co.jp>
2022-02-03 13:19:58 +0900

    ADD: task登録フォーム
    簡単なフォームを作成。bootstrap未導入。後でviewを編集する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
     @tasks = Task.all
   end
 
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to root_path, success: t('defaults.messages.created', item: Task.model_name.human)
+      redirect_to root_path
     else
       flash.now['danger'] = t('defaults.messages.not_created', item: Task.model_name.human)
     end
